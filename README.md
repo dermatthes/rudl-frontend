2b55ff41b1cdea7851f2e71434418ad709bff53d
matthes <matthes@leuffen.de>
2017-02-10 02:50:09 +0100

    Testing udp messages for logging

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # udplog
 UDP Logging Sender/Receiver
+
+
+## 
---
+++ src/UdpLogConnector.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthes
+ * Date: 10.02.17
+ * Time: 02:27
+ */
+
+    namespace Rudl;
+
+
+    class UdpLogConnector
+    {
+
+
+        public function __construct($con)
+        {
+        }
+
+
+        /**
+         * Push statistics
+         */
+        public function stat()
+        {
+
+        }
+
+
+        public function log() {
+
+        }
+
+        public function error() {
+
+        }
+
+        public function emerg() {
+
+        }
+
+
+        public function exception() {
+
+        }
+
+
+
+
+
+
+    }
